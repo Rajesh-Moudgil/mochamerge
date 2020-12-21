bb2d879864be407221a5e902fa199eee3e7082d2
Rajesh Kumar Moudgil <r3.kumar@gmail.com>
2020-12-20 22:28:08 -0500

    Working code... still raw though

--- app.js
+++ app.js
@@ -1,18 +1,87 @@
 const argv = require('minimist')(process.argv.slice(2));
+const fs=require('fs');
+const path = require('path');
 const { min, max } = require('lodash');
 const { TestReport, Test, Suite } = require('mochawesome-report-generator/bin/types.js');
 const f = require('./lib/files.js');
 const mUtils = require('./lib/mocha-utils.js');
 
+console.log(argv);
+let argsValidated=true;
+let error=null;
+
+if(argv._ && argv._.length > 0 && argv._.length < 3){
+    argsValidated = argv._.length === 1 && argv.e && argv.o
+        && (argv.e === 'failed' || argv.e === 'skipped')
+        && (argv.o.length > 2);
+    
+    if(! argsValidated){
+        argsValidated = argv._.length === 2 ;
+    }
+
+}
+
+if(!argsValidated){
+    showErrorUsage();
+} else {
+    // proceed if all is good
+    if(argv._.length === 1){
+        extractTestSuites(argv._[0],argv.e,argv.o);
+    } else {
+        let outputFilename = argv._.length === 3 ? argv._[2]:argv._[0];
+        mergeMochaJsonReports(argv._[0],argv._[1], outputFilename )
+    }
+}
+
+function showErrorUsage() {
+    console.error(` ERROR in command...
+
+    USAGE:
+        Extract failed or skipped test suites to another directory:
+            mochamerge <input json filename> -e <failed | skipped> -o <output dir>
+        Merge 2 reports
+            mochamerge <main json report> <second json report> [-o <output filename>]
+    `);
+};
+
+
+
+function extractTestSuites(filename, type, dir){
+    //verify dir exists or created
+    if(! f.pathExists(dir)){
+        f.createDir(dir);
+    }
+    if(!f.isDir(dir)){
+        throw new Error("Output directory '"+dir+"' is not accessible or not a directory.");
+    }
+
+    let report = mUtils.getReportFromFile(filename);
+    mUtils.traverseSuites(report.results,(sts,i,s) =>{
+        if( (type === 'failed' && mUtils.hasSuiteFailures(s))
+            || (type === 'skipped' && mUtils.hasSuiteSkipped(s))){
+            let source=s.fullFile;
+            let dest=path.join(dir,f.extractFilenname(source));
+            fs.copyFileSync(source,dest);
+            console.log("copy file:"+source+"  to:"+dest);
+        }
+    }, (sts,i,s) => {
+        return (type === 'failed' && mUtils.hasSuiteFailures(s))
+        || (type === 'skipped' && mUtils.hasSuiteSkipped(s))}
+    , null);
+};
+
 
 function mergeMochaJsonReports(mainFile,secondFile,outputFile){
+    if(!outputFile){
+        outputFile = mainFile;
+    }
     // read files as structure
-    const mainReport=JSON.parse( f.readFile(mainFile));
-    const secondReport=JSON.parse( f.readFile(secondFile));
+    const mainReport = mUtils.getReportFromFile(mainFile);
+    const secondReport=mUtils.getReportFromFile(secondFile);
 
     let filenameKey = (suite) => {
         if(suite && suite.fullFile && suite.fullFile.trim().length > 0){
-            return suite.fullFile.replace(/^.*[\\\/]/, '');
+            return f.extractFilenname( suite.fullFile);
         }
         return null;
     };
@@ -64,6 +133,7 @@ function mergeMochaJsonReports(mainFile,secondFile,outputFile){
     mainReport.stats=stats;
     f.writeFile(outputFile, JSON.stringify( mainReport));
 }
+
 // let totalSuites=0;
 
 // let suitesCounter =(suite) => {
